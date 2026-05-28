d81200af49822b1bcca55505aa5df723ece6b0aa
G3RGES <georgios.nashaat@gmail.com>
2026-05-28 10:39:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "1995",
+  port: 5432,
+});
 
 const app = express();
 const port = 3000;
+
+db.connect();
 
 let quiz = [
   { country: "France", capital: "Paris" },
